4c1420ce760f2489b11dd085e7718cb738c364e8
Damianpiazz <damipiazza333@gmail.com>
2025-10-05 18:43:13 -0300

    feat: Agregue el ejercicio practico de la (clase 6)

--- src/main/java/org/example/c6/Main.java
+++ src/main/java/org/example/c6/Main.java
@@ -1,4 +1,38 @@
 package org.example.c6;
 
+import org.example.c6.clases.Cliente;
+import org.example.c6.clases.Producto;
+import org.example.c6.clases.Carrito;
+
+
 public class Main {
+    public static void main(String[] args) {
+
+        // Crear Clientes
+        Cliente cliente1 = new Cliente("Sofía", "sofia@techlab.com");
+        Cliente cliente2 = new Cliente("Carlos", "carlos.com"); // email invalido
+        cliente2.setEmail("carlos@techlab.com");
+
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+
+        // Crear productos
+        Producto p1 = new Producto("Café Americano", 120, 50);
+        Producto p2 = new Producto("Té Chai", 150, 30);
+        Producto p3 = new Producto("Capuccino", 200, -10); // stock invalido
+
+        // Crear carrito
+        Carrito carrito = new Carrito();
+        carrito.agregarProducto(p1);
+        carrito.agregarProducto(p2);
+        carrito.agregarProducto(p3);
+
+        carrito.mostrarProductos();
+
+        // Mostrar total del carrito
+        System.out.println("\nTotal del carrito: $" + carrito.calcularTotal());
+
+        // Mostrar cuantos productos se crearon
+        System.out.println("\nTotal de productos creados: " + Producto.getContadorProductos());
+    }
 }
--- src/main/java/org/example/c6/clases/Carrito.java
+++ src/main/java/org/example/c6/clases/Carrito.java
@@ -1,4 +1,35 @@
 package org.example.c6.clases;
 
+import java.util.ArrayList;
+
 public class Carrito {
+    private final ArrayList<Producto> productos;
+
+    // Constructor
+    public Carrito() {
+        productos = new ArrayList<>();
+    }
+
+    // Agregar producto
+    public void agregarProducto(Producto producto) {
+        productos.add(producto);
+        System.out.println("Producto agregado: " + producto.getNombre());
+    }
+
+    // Calcular el total
+    public double calcularTotal() {
+        double total = 0;
+        for (Producto p : productos) {
+            total += p.getPrecio();
+        }
+        return total;
+    }
+
+    // Mostrar productos
+    public void mostrarProductos() {
+        System.out.println("\nProductos en el carrito:");
+        for (Producto p : productos) {
+            System.out.println("- " + p);
+        }
+    }
 }
